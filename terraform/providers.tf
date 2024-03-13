ef7d88e1b31267ea60ada712682ff30860ecf899
Wellyson Freitas <wellyson.freitas@n26.com>
2024-03-13 20:41:35 +0100

    Dummy

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -16,14 +16,3 @@ terraform {
 provider "aws" {
   region = var.region
 }
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
-    command     = "aws"
-  }
-}
