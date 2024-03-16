e2fd638833e396d432284963c7040d10ffb7b060
Wellyson Freitas <wellyson.freitas@n26.com>
2024-03-16 11:28:59 +0100

    Update IaC

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -4,12 +4,30 @@ module "ecr" {
   source  = "terraform-aws-modules/ecr/aws"
   version = "1.6.0"
 
-  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
-
   repository_name = "tech-challenge"
+
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
 
   repository_force_delete = true
 
   repository_image_tag_mutability = "MUTABLE"
   repository_image_scan_on_push   = false
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
